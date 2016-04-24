37e987315459c00a47fff256bbc5e400a16b22a0
Martin Stancanelli <mastanca14@gmail.com>
2016-04-23 22:49:00 -0300

    Fix clear stream before new imput in client

--- src/Client/client_Client.cpp
+++ src/Client/client_Client.cpp
@@ -33,6 +33,7 @@ void Client::run() {
 	std::stringstream inputStream;
 	while (std::getline(std::cin, inputLine)) {
 		// Get line from stdin and separate to variables
+		inputStream.clear();
 		inputStream.str(inputLine);
 		processInput(city, temperature, day, inputStream);
 
@@ -42,6 +43,7 @@ void Client::run() {
 		mapper.map(city, temperature, day, mappedStream);
 
 		serverProxy.send(mappedStream.str() + "\n");
+		std::cout << "Sent " << mappedStream.str() << std::endl;
 	}
 	serverProxy.send(STOP_SENDING_SIGNAL);
 }
