be689e83299a9c437e23f26cbd587fd46cccadfc
Martin Stancanelli <mastanca14@gmail.com>
2016-05-08 16:51:05 -0300

    Remove select method from socket
    Add terminate method

--- src/server_AcceptorWorker.cpp
+++ src/server_AcceptorWorker.cpp
@@ -35,24 +35,26 @@ AcceptorWorker::~AcceptorWorker() {
 
 void AcceptorWorker::run() {
 	while (*keepOnListening) {
-		// If there are connections available...
-		int availableConnections = dispatcherSocket->select();
-		if (availableConnections > 0) {
-			for (int i = 0; i < availableConnections; ++i) {
-				ClientProxy* client = new ClientProxy;
-				clients.push_back(client);
-				client->acceptNewConnection(*dispatcherSocket);
-				if (client->isConnected()) {
-					// Spawn a receiver worker
-					// It will call our client proxy's receive method
-					ReceiverWorker* receiverWorker = new ReceiverWorker(client,
-							mappedData);
-					launchedThreads.push_back(receiverWorker);
-					receiverWorker->start();
-				}
-			}
+		ClientProxy* client = new ClientProxy;
+		clients.push_back(client);
+		client->acceptNewConnection(*dispatcherSocket);
+		if (client->isConnected()) {
+			// Spawn a receiver worker
+			// It will call our client proxy's receive method
+			ReceiverWorker* receiverWorker = new ReceiverWorker(client,
+					mappedData);
+			launchedThreads.push_back(receiverWorker);
+			receiverWorker->start();
 		}
 	}
+}
+
+void AcceptorWorker::terminate(){
+	// This method was created to avoid the use of select, hoping it was
+	// the best workaround
+	// We simply call it when the server receives the STOP_LISTENING signal
+	// It closes the dispatcher socket and joins the receiver threads
+	dispatcherSocket->terminate();
 
 	// We are done listening, so join my children
 	for (std::vector<Thread*>::iterator it = launchedThreads.begin();
